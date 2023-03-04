7f4f479614e286fa3df04d5d03c49da9b2f3b15e
Kamil Szafrański <107311803+KamilSzafranski@users.noreply.github.com>
2023-03-04 17:16:26 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { Sheet } from "./Sheet/Sheet";
+import { Contacts } from "./Contacts/Contacts";
+import css from "./App.module.css";
+import { nanoid } from "nanoid";
+import { getStorage, saveStorage } from "../utils/utils.js";
+import { useSelector } from "react-redux";
+
 export const App = () => {
+  const contact = useSelector(state => state.phoneBook);
+
+  useEffect(() => {
+    if (contact?.length) {
+      saveStorage("contacts", contact);
+    }
+  }, [contact]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <Sheet />
+      <Contacts />
     </div>
   );
 };
